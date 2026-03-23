ce035487e20058106f583b51f3993c50ff386664
Ikram-debug12 <i.aitmazouz4699@uca.ac.ma>
2026-03-23 01:28:51 +0100

    ajout src et public

---
+++ src/components/TodoItem.js
@@ -0,0 +1,15 @@
+function TodoItem({ tache, onToggle, onDelete }) {
+  return (
+    <li className={`todo-item ${tache.terminee ? 'terminee' : ''}`}>
+      <input
+        type="checkbox"
+        checked={tache.terminee}
+        onChange={() => onToggle(tache.id)}
+      />
+      <span className="todo-texte">{tache.texte}</span>
+      <button onClick={() => onDelete(tache.id)}>🗑️</button>
+    </li>
+  );
+}
+
+export default TodoItem;
